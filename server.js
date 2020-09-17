ce66a85d8acb384929dc1f852d1f7675382d6ddc
N1Anoobis <mrbond84@hotmail.com>
2020-09-17 11:43:39 +0200

    Move server and packed.jason to main folder

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 const app = express();
 
 const messages =[];
-app.use(express.static(__dirname));
+app.use(express.static(`${__dirname}/client`));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))});
+    res.sendFile(path.join(__dirname, '/client/index.html'))});
 
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
